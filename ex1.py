ba5979cee33f892c8fe4c052d9d6986a28400470
Sean Maguire <smaguire143@gmail.com>
2017-02-24 19:17:15 -0500

    Here's a commit messsage for the initial push

---
+++ ex1.py
@@ -0,0 +1,8 @@
+# Example #1 from the learn python the hard way website book
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "this is fun."
+print 'Yay! Printer.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
